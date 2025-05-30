39b8f07807706ef22d41c8075c55f33f6e384403
Wender1903 <wenderman74@gmail.com>
2025-05-30 20:29:27 -0300

    :sparkles: feat: Adicionando navegação para o quiz da 1° e 3° Série do Ensino Médio

--- app/src/main/java/com/example/myapplication/EscolherSerieActivity.kt
+++ app/src/main/java/com/example/myapplication/EscolherSerieActivity.kt
@@ -6,19 +6,32 @@ import android.os.Bundle
 import android.widget.Button
 
 class EscolherSerieActivity : AppCompatActivity() {
-    lateinit var abrirBotao: Button
+    lateinit var BotaoSeire1: Button
+    lateinit var BotaoSeire2: Button
+    lateinit var BotaoSeire3: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.escolher_serie)
+        BotaoSeire1 = findViewById( R.id.primeira_serie)
+        BotaoSeire2 = findViewById( R.id.segunda_serie)
+        BotaoSeire3 = findViewById( R.id.terceira_serie)
 
-        abrirBotao = findViewById( R.id.segunda_serie)
-        abrirBotao.setOnClickListener() {
-
-            val intent = Intent(
-                this,
-                Questoes2serieActivity::class.java
-            )
+        BotaoSeire1.setOnClickListener {
+            val intent = Intent(this, Questoes1SerieActivity::class.java)
             startActivity( intent )
         }
+
+        BotaoSeire2.setOnClickListener() {
+            val intent = Intent(this,  Questoes2serieActivity::class.java)
+            startActivity( intent )
+        }
+
+        BotaoSeire3.setOnClickListener() {
+            val intent = Intent (this, Questoes3serieActivity::class.java)
+            startActivity( intent )
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Questoes1SerieActivity.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Questoes1SerieActivity : AppCompatActivity() {
+
+    lateinit var botaoFechar: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.questoes1_serie)
+
+        botaoFechar = findViewById(R.id.retornar)
+        botaoFechar.setOnClickListener {
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
