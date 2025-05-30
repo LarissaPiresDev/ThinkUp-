ed398cb0e39cbd22ac001222465714555c807787
pablobmm <pablobarros832@gmail.com>
2025-05-30 11:17:58 -0300

    transição da tela inicial para a tela escolher serie funcionando

---
+++ app/src/main/java/com/example/myapplication/EscolherSerieActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EscolherSerieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.escolher_serie) // Certifique-se que esse layout existe
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.tela_inicial)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Referenciar o botão pelo ID
+        val button = findViewById<Button>(R.id.button)
+
+        // Criar a ação de clique
+        button.setOnClickListener {
+            val intent = Intent(this, EscolherSerieActivity::class.java)
+            startActivity(intent)
         }
     }
 }
