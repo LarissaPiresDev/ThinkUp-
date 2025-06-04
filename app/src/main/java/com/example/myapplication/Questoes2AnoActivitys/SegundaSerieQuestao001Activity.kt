c44a35d5abeeadba89fa21a97cbbcfc843b7196e
Wender1903 <wenderman74@gmail.com>
2025-06-04 19:26:47 -0300

    :sparkles: feat: Configurando questão 1 do questionário do Segundo Ano do Ensino Médio (completado)

--- app/src/main/java/com/example/myapplication/Questoes2AnoActivitys/SegundaSerieQuestao001Activity.kt
+++ app/src/main/java/com/example/myapplication/Questoes2AnoActivitys/SegundaSerieQuestao001Activity.kt
@@ -1,17 +1,77 @@
 package com.example.myapplication.Questoes2AnoActivitys
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.Pontuacao
+import com.example.myapplication.Questoes1AnoActivitys.PrimeiraSerieQuestao003Activity
 import com.example.myapplication.R
 
 class SegundaSerieQuestao001Activity : AppCompatActivity() {
+
+    lateinit var alternativa1: Button
+    lateinit var alternativa2: Button
+    lateinit var alternativa3: Button
+    lateinit var alternativa4: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.segunda_serie_questao001)
+
+        alternativa1 = findViewById(R.id.alt1)
+        alternativa2 = findViewById(R.id.alt2)
+        alternativa3 = findViewById(R.id.alt3)
+        alternativa4 = findViewById(R.id.alt4)
+
+        alternativa1.setOnClickListener {
+            alternativa1.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+            alternativa3.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, SegundaSerieQuestao002Activity::class.java)
+                startActivity(intent)
+            }, 500)
+        }
+
+        alternativa2.setOnClickListener {
+            alternativa2.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+            alternativa3.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, SegundaSerieQuestao002Activity::class.java)
+                startActivity(intent)
+
+            }, 500)
+        }
+
+        alternativa3.setOnClickListener {
+            Pontuacao.acertos += 1
+            alternativa3.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, SegundaSerieQuestao002Activity::class.java)
+                startActivity(intent)
+            }, 500)
+        }
+
+        alternativa4.setOnClickListener {
+            alternativa4.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+            alternativa3.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this, SegundaSerieQuestao002Activity::class.java)
+                startActivity(intent)
+            }, 500)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
