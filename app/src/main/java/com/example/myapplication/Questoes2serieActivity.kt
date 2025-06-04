9af29672bda8d99e225c01974847a696010c6a79
Wender1903 <wenderman74@gmail.com>
2025-06-04 19:26:07 -0300

    :sparkles: feat: Configurando questão 1 do questionário do Segundo Ano do Ensino Médio

--- app/src/main/java/com/example/myapplication/Questoes2serieActivity.kt
+++ app/src/main/java/com/example/myapplication/Questoes2serieActivity.kt
@@ -1,19 +1,30 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.Questoes2AnoActivitys.SegundaSerieQuestao001Activity
 
 class Questoes2serieActivity : AppCompatActivity() {
 
     lateinit var botaoFechar: Button
+    lateinit var iniciarQuiz2ano: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.questoes2serie)
+        iniciarQuiz2ano = findViewById( R.id.iniciarQuizPrimeiroAno)
+
+        iniciarQuiz2ano.setOnClickListener() {
+            val intent = Intent(this, SegundaSerieQuestao001Activity::class.java)
+            startActivity( intent )
+
+
+        }
 
         botaoFechar = findViewById(R.id.retornar)
         botaoFechar.setOnClickListener {
